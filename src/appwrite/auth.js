3cce7ba3c745407c19510073c437be18952baeb6
Your Name <rishabhyadav70070@gmail.com>
2024-10-06 18:21:49 +0530

    complete authentication as service

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -17,6 +17,7 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
                 //call another method
+                return this.login({email, password});
             } else {
                 return userAccount;
             }
@@ -24,6 +25,34 @@ export class AuthService {
             throw error;
         }
     }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+
+        } catch (error) {
+            return error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            //throw error;
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+
 
 }
 
