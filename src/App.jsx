364da9750d8d8dcc868db531024ad264abe063f2
Your Name <rishabhyadav70070@gmail.com>
2024-10-12 16:57:47 +0530

    Now Pages Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from "./appwrite/auth"
 import  {login, logout} from "./store/authSlice"
+import { Header , Footer } from './components' 
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const[loading, setLoading] = useState(true)
@@ -20,11 +22,18 @@ function App() {
     .finally(() => setLoading(false))
   }, [])
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between
+    bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
