ddd571f2c7d4a3c661a49b131b560bc6ac689dbb
Your Name <rishabhyadav70070@gmail.com>
2024-11-17 11:55:23 +0530

    Real time Editor

---
+++ src/components/RTE.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+
+export default function RTE() {
+  return (
+    <Editor
+    initialValue='default value'
+    init={
+        {branding:false,
+         height:500,
+         menubar:true,
+         plugins:['advlist'],   
+         toolbar:'undo redo'
+        }
+    }
+    />
+  )
+}
+
