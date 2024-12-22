f0b57f32fe53c9416201baefb5f28170770764bd
Your Name <rishabhyadav70070@gmail.com>
2024-12-22 13:20:35 +0530

    debugging indexfile

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Input, RTE, Select } from "../Index.js";
+import { Button, Input, RTE, Select } from "../index.js";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
